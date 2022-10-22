c4aefd3e96f63928e70ee0a6a0d7088280242002
Smarika Ghimire <smarikaghimire35@gmail.com>
2022-10-22 22:36:06 +0545

    1st commit

---
+++ form.php
@@ -0,0 +1,40 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+$email_from = 'info@yourwebsite.com';
+$email_subject = 'new form submission';
+$email_body = "User Name: $name.\n".
+               
+           "User email: $visitor_email.\n".
+           "Subject: $subject.\n".
+        "User message: $message.\n";
+
+    $to='smarikaghimire35@gmail.com';
+    $headers="from: $email_from \r\n";
+    $headers .= "Reply-to: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("location: contact.html");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
